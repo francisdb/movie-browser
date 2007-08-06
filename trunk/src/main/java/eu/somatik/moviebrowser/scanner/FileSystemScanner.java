66dac41c514648d6b6e2f339e519ed3571948bde
francisdb <francisdb@06e651c2-8a29-0410-9275-ff94c4ebd23e>
2007-08-06 21:38:11 +0000

    Moved some filesystem stuff to new class

---
+++ trunk/src/main/java/eu/somatik/moviebrowser/scanner/FileSystemScanner.java
@@ -0,0 +1,106 @@
+package eu.somatik.moviebrowser.scanner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @author francisdb
+ *
+ */
+public class FileSystemScanner {
+
+    private static final String IMDB_URLS[] = {
+        "http://www.imdb.com/title/",
+        "http://us.imdb.com/title/",
+        "http://www.imdb.com/title?",
+        "http://us.imdb.com/title?",
+        "http://imdb.com/title/"
+    };
+	 
+    
+    /**
+     * Locates she sample file and returns it
+     * @param folder
+     * @return the sample file or null if not found 
+     */
+    public File findSample(File folder){
+    	File sample = null;
+    	for(File file:folder.listFiles()){
+    		if("sample".equals(file.getName().toLowerCase())){
+    			for(File sampleFolderFile:file.listFiles()){
+    				if(sampleFolderFile.getName().toLowerCase().endsWith(".avi")){
+    					sample = sampleFolderFile;
+    				}
+    			}
+    		}
+    	}
+    	return sample;
+    }
+    
+    /**
+     * Finds the NFO file and looks for the imdb url inside it
+     * @param dir
+     * @return the nfo URL or null
+     */
+    public String findNfoUrl(File dir){
+        String url = null;
+        System.out.println("looking for nfo in "+dir.getPath());
+        for(File file:dir.listFiles()){
+            if(file.getName().toLowerCase().endsWith(".nfo")){
+                System.out.println("checking nfo: "+file.getName());
+                url = findImdbUrl(file); 
+            }
+        }
+        
+        return url;
+    }
+    
+    private String findImdbUrl(File nfoFile){
+    	String url = null;
+    	try{
+            FileInputStream fis = new FileInputStream(nfoFile);
+            int x= fis.available();
+            byte b[]= new byte[x];
+            fis.read(b);
+            
+            String content = new String(b).toLowerCase();
+            int start = -1;
+            int i = 0;
+            String urlStart = null;
+            while(start == -1 && i < IMDB_URLS.length){
+                urlStart = IMDB_URLS[i];
+                //System.out.println("looking for "+urlStart);
+                start = content.indexOf(urlStart);
+                i++;
+            }
+            
+            if(start != -1){
+                i = start + urlStart.length();
+                int end = -1;
+                char character;
+                while(i < content.length()-1 && end == -1){
+                    character = content.charAt(i);
+                    //System.out.println((int)character);
+                    if(character == '\n' || character == '\t' || character == '\n' || character == (char)32){
+                        end = i;
+                    }
+                    i++;
+                }
+                
+                //if the url is the end of the file
+                if(i == content.length()-1){
+                    end = content.length()-1;
+                }
+                
+                url = content.substring(start,end);
+                System.out.println("IMDB url found: "+url);
+            }
+            
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+        return url;
+    }
+    
+}
