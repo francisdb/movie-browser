e27cadc5f021e7d10b6472015a647c11830f19ca
francisdb <francisdb@06e651c2-8a29-0410-9275-ff94c4ebd23e>
2008-07-17 22:23:11 +0000

    junit test for the score calculator

---
+++ src/test/java/eu/somatik/moviebrowser/service/ScoreCalculatorTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.somatik.moviebrowser.service;
+
+import eu.somatik.moviebrowser.domain.Movie;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author francisdb
+ */
+public class ScoreCalculatorTest {
+
+    /**
+     * Test of calculate method, of class ScoreCalculator.
+     */
+    @Test
+    public void testCalculate() {
+        Movie movie = new Movie();
+        ScoreCalculator instance = new ScoreCalculator();
+        assertNull("Result should be null when no data", instance.calculate(movie));
+        
+        movie.setImdbScore(32);
+        assertEquals(Integer.valueOf(32), instance.calculate(movie));
+        
+        movie.setTomatoScore(32);
+        movie.setMovieWebScore(32);
+        assertEquals(Integer.valueOf(32), instance.calculate(movie));
+        
+        movie.setTomatoScore(80);
+        assertEquals(Integer.valueOf((32*3+80)/4), instance.calculate(movie));
+        
+    }
+
+}
