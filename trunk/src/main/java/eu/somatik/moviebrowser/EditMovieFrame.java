cea0528bd8bf353446fd29406abcc413f20fd907
ravi.undupitiya <ravi.undupitiya@06e651c2-8a29-0410-9275-ff94c4ebd23e>
2008-07-18 19:33:59 +0000

    Issue Number 18: minor changes, missing @Override.

--- trunk/src/main/java/eu/somatik/moviebrowser/EditMovieFrame.java
+++ trunk/src/main/java/eu/somatik/moviebrowser/EditMovieFrame.java
@@ -6,10 +6,17 @@
 
 package eu.somatik.moviebrowser;
 
+import java.util.Iterator;
+import java.util.List;
+import javax.swing.DefaultListModel;
+import java.net.URLEncoder;
+        
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
 import au.id.jericho.lib.html.HTMLElementName;
 import au.id.jericho.lib.html.Source;
+import au.id.jericho.lib.html.Element;
 
 /**
  *
@@ -18,8 +25,9 @@ import au.id.jericho.lib.html.Source;
 public class EditMovieFrame extends javax.swing.JFrame {
 
     /** Creates new form EditMovieFrame */
-    public EditMovieFrame() {
+    public EditMovieFrame(String searchkey) {
         initComponents();
+        searchTextField.setText(searchkey);
     }
 
     /** This method is called from within the constructor to
@@ -37,6 +45,8 @@ public class EditMovieFrame extends javax.swing.JFrame {
         jSeparator1 = new javax.swing.JSeparator();
         jScrollPane1 = new javax.swing.JScrollPane();
         resultsList = new javax.swing.JList();
+        updateButton = new javax.swing.JButton();
+        statusProgressBar = new javax.swing.JProgressBar();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Edit Movie");
@@ -50,66 +60,118 @@ public class EditMovieFrame extends javax.swing.JFrame {
             }
         });
 
-        resultsList.setModel(new javax.swing.AbstractListModel() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public Object getElementAt(int i) { return strings[i]; }
-        });
         resultsList.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         jScrollPane1.setViewportView(resultsList);
+
+        updateButton.setText("Update");
+
+        statusProgressBar.setForeground(new java.awt.Color(255, 153, 51));
+        statusProgressBar.setString("");
+        statusProgressBar.setStringPainted(true);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addComponent(searchLabel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(searchTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap()
+                .addComponent(searchLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(searchTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 256, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(searchButton, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(19, Short.MAX_VALUE))
-            .addComponent(jSeparator1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+                .addContainerGap())
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 384, Short.MAX_VALUE)
                 .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(statusProgressBar, javax.swing.GroupLayout.DEFAULT_SIZE, 309, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(updateButton)
+                .addContainerGap())
+            .addComponent(jSeparator1, javax.swing.GroupLayout.DEFAULT_SIZE, 404, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(searchButton)
                     .addComponent(searchLabel)
-                    .addComponent(searchTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(searchButton))
+                    .addComponent(searchTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 233, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(statusProgressBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(updateButton))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
 private void searchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchButtonActionPerformed
-
+    try {
+        getResults();
+    } 
+    catch (Exception ex) {
+        System.out.println(ex);
+    }
 }//GEN-LAST:event_searchButtonActionPerformed
 
  private Source getRequest(String searchkey) throws Exception {
 
         HttpClient client = new HttpClient();
-        //HttpMethod method = new GetMethod("http://www.imdb.com/find?q=" + searchTextField.getText());
-        //client.executeMethod(method);
+        HttpMethod method = new GetMethod("http://www.imdb.com/find?q=" + URLEncoder.encode(searchTextField.getText(), "UTF-8"));
+        statusProgressBar.setIndeterminate(true);
+        client.executeMethod(method);
         
         Source source = null;
-        //source = new Source(method.getResponseBodyAsString());
+        source = new Source(method.getResponseBodyAsString());
         //source.setLogWriter(new OutputStreamWriter(System.err)); // send log messages to stderr
-        //source.fullSequentialParse();
+        source.fullSequentialParse();
         return source;
     }
+ 
+ private void getResults() throws Exception {
+        statusProgressBar.setString("Searching...");
+        DefaultListModel listModel = new DefaultListModel();
+        
+        Source source = null;
+        source = getRequest(searchTextField.getText());
+        System.out.println(source);
+        Element titleElement = (Element) source.findAllElements(HTMLElementName.TITLE).get(0);
+        
+        if (titleElement.getContent().getTextExtractor().toString().contains("IMDb Search")) {
+            List<?> linkElements = source.findAllElements(HTMLElementName.A);
+            for (Iterator<?> i = linkElements.iterator(); i.hasNext();) {
+                Element linkElement = (Element) i.next();
+                String href = linkElement.getAttributeValue("href");
+                System.out.println(href);
+                if (href != null && href.startsWith("/title/tt")) {
+                    String name = linkElement.getContent().toString();
+                    int questionMarkIndex = href.indexOf('?');
+                    if (questionMarkIndex != -1) {
+                        href = href.substring(0, questionMarkIndex);
+                    }
+                    listModel.addElement(name + " ~ " + "http://www.imdb.com" + href);
+                    titleElement = (Element) source.findAllElements(HTMLElementName.TITLE).get(0);
+                }
+            }
+
+        }
+        resultsList.setModel(listModel);
+        statusProgressBar.setIndeterminate(false);
+        if(listModel.isEmpty())
+            statusProgressBar.setString("No Results Found");
+        else
+            statusProgressBar.setString("");
+ }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane jScrollPane1;
@@ -118,6 +180,8 @@ private void searchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-
     private javax.swing.JButton searchButton;
     private javax.swing.JLabel searchLabel;
     private javax.swing.JTextField searchTextField;
+    private javax.swing.JProgressBar statusProgressBar;
+    private javax.swing.JButton updateButton;
     // End of variables declaration//GEN-END:variables
 
 }
