23a7aa3da413b8028e08e2f866edff1593afefdb
ravi.undupitiya <ravi.undupitiya@06e651c2-8a29-0410-9275-ff94c4ebd23e>
2008-08-03 21:44:18 +0000

    passing of imdbID to SubtitleCrawlerFrame class.

--- trunk/src/main/java/eu/somatik/moviebrowser/gui/SubtitleCrawlerFrame.java
+++ trunk/src/main/java/eu/somatik/moviebrowser/gui/SubtitleCrawlerFrame.java
@@ -6,9 +6,38 @@
 
 package eu.somatik.moviebrowser.gui;
 
+import javax.swing.table.DefaultTableModel;
 import javax.swing.ImageIcon;
-import java.util.List;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingUtilities;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import java.awt.Desktop;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.id.jericho.lib.html.Element;
+import au.id.jericho.lib.html.HTMLElementName;
+import au.id.jericho.lib.html.Source;
+import com.google.inject.Inject;
+
+import eu.somatik.moviebrowser.domain.Movie;
+import eu.somatik.moviebrowser.service.HttpSourceLoader;
 
 /**
  *
@@ -16,14 +45,24 @@ import java.util.Iterator;
  */
 public class SubtitleCrawlerFrame extends javax.swing.JFrame {
 
+    private DefaultTableModel model;
+    private static final Logger LOGGER = LoggerFactory.getLogger(SubtitleCrawlerFrame.class);
+    private final HttpSourceLoader httpLoader;
+    
     /** Creates new form SubtitleCrawlerFrame */
-    public SubtitleCrawlerFrame(List<String> files) {
+    public SubtitleCrawlerFrame(List<String> files, String imdbID, HttpSourceLoader httpLoader) {
+        this.httpLoader = httpLoader;
+        
+        //Prepare table model
+        String[] columnNames = {"Language", "File Name", "Type", "Source"};
+        model = new DefaultTableModel(null, columnNames);
+        
         initComponents();
-        this.setTitle("Subtitle Crawler (" + files.toString() + ")");
-        crawl(files);
+        this.setTitle("Subtitle Crawler " + files.toString());
+        crawl(files, imdbID);
         searchButton.setEnabled(false);
         
-    }
+    };
 
     /** This method is called from within the constructor to
      * initialize the form.
@@ -49,30 +88,16 @@ public class SubtitleCrawlerFrame extends javax.swing.JFrame {
 
         infoLabel.setText("Double click on the subtitle you require below to begin download. Use the search box to refine your search.");
 
-        subtitlesTable.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null}
-            },
-            new String [] {
-                "Language", "Name", "Type", "Source"
+        subtitlesTable.setModel(model);
+        subtitlesTable.setColumnSelectionAllowed(true);
+        subtitlesTable.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                subtitlesTableMouseClicked(evt);
             }
-        ) {
-            Class[] types = new Class [] {
-                java.lang.Object.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
-            };
-            boolean[] canEdit = new boolean [] {
-                false, false, false, false
-            };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                subtitlesTableMousePressed(evt);
             }
         });
-        subtitlesTable.setColumnSelectionAllowed(true);
         jScrollPane1.setViewportView(subtitlesTable);
         subtitlesTable.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_INTERVAL_SELECTION);
         subtitlesTable.getColumnModel().getColumn(0).setPreferredWidth(5);
@@ -80,6 +105,11 @@ public class SubtitleCrawlerFrame extends javax.swing.JFrame {
         subtitlesTable.getColumnModel().getColumn(2).setPreferredWidth(10);
 
         searchButton.setText("Search");
+        searchButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                searchButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -88,13 +118,13 @@ public class SubtitleCrawlerFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 515, Short.MAX_VALUE)
                     .addComponent(statusProgressBar, javax.swing.GroupLayout.DEFAULT_SIZE, 515, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(searchText, javax.swing.GroupLayout.DEFAULT_SIZE, 438, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(searchButton))
-                    .addComponent(infoLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(infoLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 515, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -105,9 +135,9 @@ public class SubtitleCrawlerFrame extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(searchButton)
                     .addComponent(searchText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(1, 1, 1)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(statusProgressBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
@@ -115,8 +145,32 @@ public class SubtitleCrawlerFrame extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-public void crawl(List<String> files) {
+private void searchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchButtonActionPerformed
+// TODO add your handling code here:
+}//GEN-LAST:event_searchButtonActionPerformed
+
+private void subtitlesTableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_subtitlesTableMouseClicked
+
+}//GEN-LAST:event_subtitlesTableMouseClicked
+
+private void subtitlesTableMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_subtitlesTableMousePressed
+if (evt.getClickCount() == 2) {
+    String[] info = (String[]) subtitlesTable.getValueAt(subtitlesTable.getSelectedRow(), subtitlesTable.convertColumnIndexToView(1));
+    if (SwingUtilities.isLeftMouseButton(evt)) {
+        try {
+            Desktop.getDesktop().browse(new URI(info[1]));
+        } catch (IOException ex) {
+            LOGGER.error("Could not open " + info[1], ex);
+        } catch (URISyntaxException ex) {
+            LOGGER.error("Could not open " + info[1], ex);
+        }
+    }
+}
+}//GEN-LAST:event_subtitlesTableMousePressed
+
+public void crawl(List<String> files, String imdbID) {
     String fileName;
+    Set<String[]> results = new HashSet<String[]>();
     Iterator<String> i = files.iterator();
     while(i.hasNext()) {
         fileName = (String) i.next();
@@ -124,9 +178,38 @@ public void crawl(List<String> files) {
         if(fileName.endsWith(".")) {
             fileName = fileName.substring(0, (fileName.length()-1));
         }
+        
         System.out.println(fileName);
+        try {
+            //Add other methods to get subs from other sources. 
+        }
+        catch (Exception ex) {
+            LOGGER.error("Error retrieveng subtitle results. ", ex);
+        }
     }
+    
+    try {
+        //Get subtitlesource
+        results.add(getSubtitleSourceResults(imdbID));
+    }
+    catch (Exception ex) {
+         LOGGER.error("Error retrieveng subtitlesource.org results. ", ex);
+    }
+    
+    Iterator<String[]> j = results.iterator();
+    String[] result;
+    while (j.hasNext()) {
+        result = (String[]) j.next();
+        model.addRow(result);
+    }
+    
+    searchButton.setEnabled(true);
 }
+
+   public String[] getSubtitleSourceResults(String imdbID) throws IOException{
+       String[] results = {"N/A", "http://www.subtitlesource.org/title/tt" + imdbID, "Various", "subtitlesource.org"};
+       return  results;
+    }
             
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel infoLabel;
