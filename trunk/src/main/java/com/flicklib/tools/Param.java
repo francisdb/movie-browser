5c0010c84b559994066971d379b9ededbb6387f1
francisdb <francisdb@06e651c2-8a29-0410-9275-ff94c4ebd23e>
2008-08-07 13:59:53 +0000

    fixed utility class

--- trunk/src/main/java/com/flicklib/tools/Param.java
+++ trunk/src/main/java/com/flicklib/tools/Param.java
@@ -12,48 +12,51 @@ import org.slf4j.LoggerFactory;
  * @author francisdb
  */
 public class Param {
-    
+
     private static final Logger LOGGER = LoggerFactory.getLogger(Param.class);
 
     private Param() {
         // Utility class
     }
-    
+
     /**
      * Generates a map of string - string key-value pairs encoded by UrlEncoder
      * The number of parameters hould be even!
      * @param paramValue
      * @return the Map
      */
-    public static final Map<String,String> map(final String...paramValue){
-        if(paramValue.length % 2 != 0){
-            throw new IllegalArgumentException("parameter count should be even but is "+paramValue.length);
+    public static final Map<String, String> map(final String... paramValue) {
+        if (paramValue.length % 2 != 0) {
+            throw new IllegalArgumentException("parameter count should be even but is " + paramValue.length);
         }
-        Map<String,String> params = new HashMap<String, String>();
-        for(int i = 0;i<paramValue.length/2;i++){
-            params.put(encode(paramValue[i]), encode(paramValue[i+1]));
+        Map<String, String> params = new HashMap<String, String>();
+        for (int i = 0; i < paramValue.length / 2; i++) {
+            params.put(encode(paramValue[i]), encode(paramValue[i + 1]));
         }
-        return new HashMap<String, String>();
+        return params;
     }
-    
-    public static final String paramString(final String...paramValue){
+
+    public static final String paramString(final String... paramValue) {
         return paramString(map(paramValue));
     }
-    
-    
-    public static final String paramString(Map<String,String> params){
+
+    public static final String paramString(Map<String, String> params) {
         StringBuilder result = new StringBuilder();
-        if(params.size() > 0){
-            result.append('?');
-            for(Map.Entry<String, String> param:params.entrySet()){
-                result.append(param.getKey());
-                result.append('=');
-                result.append(param.getValue());
+        boolean first = true;
+        for (Map.Entry<String, String> param : params.entrySet()) {
+            if (first) {
+                result.append('?');
+            } else {
+                result.append('&');
             }
+            result.append(param.getKey());
+            result.append('=');
+            result.append(param.getValue());
         }
+
         return result.toString();
     }
-    
+
     /**
      * UrlEncode a string
      * @param str 
@@ -68,5 +71,4 @@ public class Param {
         }
         return encoded;
     }
-
 }
