24278311445e4b417460a2665c868d5e4ddfe268
francisdb <francisdb@06e651c2-8a29-0410-9275-ff94c4ebd23e>
2008-08-07 13:47:28 +0000

    preparing to split flicklib

---
+++ trunk/src/main/java/com/flicklib/service/movie/imdb/ImdbTrailerFinder.java
@@ -0,0 +1,19 @@
+package com.flicklib.service.movie.imdb;
+
+import eu.somatik.moviebrowser.service.imdb.*;
+import com.flicklib.api.TrailerFinder;
+import eu.somatik.moviebrowser.domain.Movie;
+
+/**
+ *
+ * @author francisdb
+ */
+public class ImdbTrailerFinder implements TrailerFinder{
+
+    @Override
+    public String findTrailerUrl(Movie movie) {
+        String url = ImdbUrlGenerator.generateImdbUrl(movie) + "trailers";
+        return url;
+    }
+
+}
