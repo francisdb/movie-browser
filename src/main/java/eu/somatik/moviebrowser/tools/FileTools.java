bdb012eaa135eec3b012cd9a830042f16270dc5d
francisdb <francisdb@06e651c2-8a29-0410-9275-ff94c4ebd23e>
2008-08-14 13:56:49 +0000

    issue 55 implemented database versioning

--- src/main/java/eu/somatik/moviebrowser/tools/FileTools.java
+++ src/main/java/eu/somatik/moviebrowser/tools/FileTools.java
@@ -1,6 +1,12 @@
 package eu.somatik.moviebrowser.tools;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,5 +43,65 @@ public class FileTools {
             }
         }
         return (path.delete());
+    }
+    
+    /**
+     * Loads a properties file
+     * @param propsFile
+     * @return the properties (empty if the propsFile does not exist)
+     */
+    public static Properties loadProperties(File propsFile){
+        Properties props = new Properties();
+        InputStream is = null;
+        try {
+            if (propsFile.exists()) {
+                is = new FileInputStream(propsFile);
+                props.load(is);
+            }
+        } catch (IOException ex) {
+            LOGGER.error("Could not load properties from " + propsFile.getAbsolutePath(), ex);
+        } catch (SecurityException ex) {
+            LOGGER.error("Could not load preferences from " + propsFile.getAbsolutePath(), ex);
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException ex) {
+                    LOGGER.error("Could not close inputstream for" + propsFile.getAbsolutePath(), ex);
+                }
+            }
+        }
+        return props;
+    }
+    
+    /**
+     * Stores a properties file, will create the path if needed
+     * @param propsFile
+     * @param the properties
+     */
+    public static void storePropeties(Properties properties, File propsFile) {
+        File parent = propsFile.getParentFile();
+        if(!parent.exists()){
+            LOGGER.debug("Recursively creating folder for properties: "+parent.getAbsolutePath());
+            parent.mkdirs();
+        }
+        propsFile.getParentFile().mkdirs();
+        OutputStream os = null;
+        try{
+            os = new FileOutputStream(propsFile);
+            properties.store(os, "Database version file");
+        }catch(IOException ex){
+            LOGGER.error("Could not save preferences to "+propsFile.getAbsolutePath(), ex);
+        }catch(SecurityException ex){
+            LOGGER.error("Could not save preferences to "+propsFile.getAbsolutePath(), ex);
+        }finally{
+            if(os!=null){
+                try{
+                    os.close();
+                }catch(IOException ex){
+                    LOGGER.error("Could not close outputstream for"+propsFile.getAbsolutePath(), ex);
+                }
+            }
+        }
     }
 }
