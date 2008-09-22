32d33c9c806742e3bdcc4fd8a4960e882ebe0a89
francisdb@gmail.com <francisdb@gmail.com@06e651c2-8a29-0410-9275-ff94c4ebd23e>
2008-09-22 09:09:47 +0000

    checkstyle fixes

--- src/main/java/eu/somatik/moviebrowser/domain/ReleaseType.java
+++ src/main/java/eu/somatik/moviebrowser/domain/ReleaseType.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of Movie Browser.
  * 
- * Copyright (C) Zsombor Gegesy
+ * Copyright (C) Francis De Brabandere
  * 
  * Movie Browser is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
