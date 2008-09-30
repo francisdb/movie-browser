2a8a35f80e6fffc6ab69ee1f90dd8e7d59a53015
francisdb@gmail.com <francisdb@gmail.com@06e651c2-8a29-0410-9275-ff94c4ebd23e>
2008-09-30 13:09:35 +0000

    added missing headers

--- src/main/java/eu/somatik/moviebrowser/gui/AppleTrailerAction.java
+++ src/main/java/eu/somatik/moviebrowser/gui/AppleTrailerAction.java
@@ -1,4 +1,22 @@
 /*
+ * This file is part of Movie Browser.
+ * 
+ * Copyright (C) Francis De Brabandere
+ * 
+ * Movie Browser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Movie Browser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -17,7 +35,7 @@ import javax.swing.JOptionPane;
  * 
  * @author zsombor
  */
-public class AppleTrailerAction extends AbstractAction {
+class AppleTrailerAction extends AbstractAction {
 
     final MainFrame mainFrame;
     final MovieBrowser browser;
