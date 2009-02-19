798ade71b4cbb7478cb911e369af2a3da7d4b0b2
francisdb <francisdb@06e651c2-8a29-0410-9275-ff94c4ebd23e>
2009-02-19 21:03:01 +0000

    some basic folder scanning tests

---
+++ trunk/src/test/java/eu/somatik/moviebrowser/service/AdvancedFolderScannerTest.java
@@ -0,0 +1,42 @@
+package eu.somatik.moviebrowser.service;
+
+import eu.somatik.moviebrowser.domain.MovieInfo;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.Before;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author francisdb
+ */
+public class AdvancedFolderScannerTest {
+
+
+    private AdvancedFolderScanner scanner;
+
+    @Before
+    public void setUp() {
+        this.scanner = new AdvancedFolderScanner(new MovieNameExtractor());
+    }
+
+    @Test
+    public void testStructure1() {
+        Set<String> folders = new HashSet<String>();
+        folders.add("target/test-classes/structure1");
+        List<MovieInfo> movies = scanner.scan(folders);
+        assertEquals(3, movies.size());
+    }
+
+    @Test
+    public void testStructure2() {
+        Set<String> folders = new HashSet<String>();
+        folders.add("target/test-classes/structure2");
+        List<MovieInfo> movies = scanner.scan(folders);
+        assertEquals(3, movies.size());
+    }
+
+}
